96732407b1a7d66f20889343dcc07b72949fd778
AswinSuresh666 <117917680+ASWINSURESH666@users.noreply.github.com>
2023-04-30 20:40:39 +0530

    Update Fun,Overload.cpp

--- Func.Overloading/Fun,Overload.cpp
+++ Func.Overloading/Fun,Overload.cpp
@@ -1,88 +1,75 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-class Complex {
-private:
-    float real;
-    float imag;
-
-public:
-    Complex() {
-        real = 0;
-        imag = 0;
-    }
-
-    void getElements() {
-        cout << "Enter the real part: ";
-        cin >> real;
-        cout << "Enter the imaginary part: ";
-        cin >> imag;
-    }
-
-    void display() {
-        if (imag == 0) {
-            cout << real << endl;
-        } else if (real == 0) {
-            cout << imag << "i" << endl;
-        } else if (imag < 0) {
-            cout << real << " - " << -imag << "i" << endl;
-        } else {
-            cout << real << " + " << imag << "i" << endl;
-        }
-    }
-
-    Complex operator +(const Complex& c) const {
-        Complex result;
-        result.real = real + c.real;
-        result.imag = imag + c.imag;
-        return result;
-    }
-
-    Complex operator *(const Complex& c) const {
-        Complex result;
-        result.real = real * c.real - imag * c.imag;
-        result.imag = real * c.imag + imag * c.real;
-        return result;
-    }
+class Vector {
+	private:
+		int i, j, k;
+		double mag;
+	public:
+		Vector(){
+			i=0;
+			j=0;
+			k=0;
+			mag=0;
+		}
+		Vector(int a, int b, int c){
+			i = a;
+			j = b;
+			k = c;
+			mag = sqrt(i*i + j*j + k*k);
+		}
+		bool operator == (Vector A){
+			return i==A.i && j==A.j && k==A.k;
+		}
+		bool operator != (Vector A){
+			return i!=A.i || j!=A.j || k!=A.k;
+		}
+		bool operator < (Vector A){
+			return mag < A.mag;
+		}
+		bool operator <= (Vector A){
+			return mag <= A.mag;
+		}
+		bool operator > (Vector A){
+			return mag > A.mag;
+		}
+		bool operator >= (Vector A){
+			return mag >= A.mag;
+		}
+		void display(){
+			cout<<i<<"i + "<<j<<"j + "<<k<<"k"<<endl;
+		}
 };
 
-int main() {
-    Complex a, b, c, d;
-    cout << "=== Complex Number 1 ===" << endl;
-    a.getElements();
-    a.display();
-    cout << "\n=== Complex Number 2 ===" << endl;
-    b.getElements();
-    b.display();
+int main(){
+	int a, b, c;
+	cout<<"Enter i for vector 1: ";
+	cin>>a;
+	cout<<"Enter j for vector 1: ";
+	cin>>b;
+	cout<<"Enter k for vector 1: ";
+	cin>>c;
+	Vector V1(a, b, c);
+	cout<<"Enter i for vector 2: ";
+	cin>>a;
+	cout<<"Enter j for vector 2: ";
+	cin>>b;
+	cout<<"Enter k for vector 2: ";
+	cin>>c;
+	Vector V2(a, b, c);
 
-    int choice;
-    do {
-        cout << "\nEnter the choice of problem:" << endl;
-        cout << "1 - Add the complex numbers" << endl;
-        cout << "2 - Multiply the complex numbers" << endl;
-        cout << "3 - Quit" << endl;
-        cin >> choice;
+	cout<<"V1: ";
+	V1.display();
+	cout<<"V2: ";
+	V2.display();
 
-        switch (choice) {
-            case 1:
-                cout << "\nThe sum is:" << endl;
-                c = a + b;
-                c.display();
-                break;
-            case 2:
-                cout << "\nThe Multiplied Ans is:" << endl;
-                d = a * b;
-                d.display();
-                break;
-            case 3:
-                cout << "\nExit" << endl;
-                break;
-            default:
-                cout << "\nentered num is wrong." << endl;
-                break;
-        }
-    } while (choice != 3);
+	cout<<"V1 == V2: "<<(V1==V2)<<endl;
+	cout<<"V1 != V2: "<<(V1!=V2)<<endl;
+	cout<<"V1 < V2: "<<(V1<V2)<<endl;
+	cout<<"V1 <= V2: "<<(V1<=V2)<<endl;
+	cout<<"V1 > V2: "<<(V1>V2)<<endl;
+	cout<<"V1 >= V2: "<<(V1>=V2)<<endl;
 
-    return 0;
+	return 0;
 }
